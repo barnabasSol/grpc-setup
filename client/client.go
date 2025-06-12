e0153d763ce5fa94e7b65fa6feb373e98c421e95
barnabasSol <barnabassolomon336@gmail.com>
2025-06-12 22:59:58 +0300

    client done but client/server communication doesnt work

---
+++ client/client.go
@@ -0,0 +1,54 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"time"
+
+	pb "github.com/barnabasSol/grpcsetup/coffeeshop_protos"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func RunClient() {
+	conn, err := grpc.NewClient(
+		"localhost:5001",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalf("failed to connect to gRPC server %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewCoffeeShopClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	menuStream, err := c.GetMenu(ctx, &pb.MenuRequest{})
+	if err != nil {
+		log.Fatalf("failed calling GetMenu %v", err)
+	}
+	done := make(chan bool)
+	var items []*pb.Item
+	go func() {
+		for {
+			resp, err := menuStream.Recv()
+			if err == io.EOF {
+				done <- true
+				return
+			}
+			if err != nil {
+				log.Fatalf("cannot receive from stream %v", err)
+			}
+			items = resp.Items
+		}
+	}()
+	<-done
+	fmt.Printf("items: %v\n", items)
+	receipt, _ := c.PlaceOrder(ctx, &pb.Order{Items: items})
+	fmt.Printf("receipt: %v\n", receipt)
+	status, _ := c.GetOrderStatus(ctx, receipt)
+	fmt.Printf("status: %v\n", status)
+
+}
--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"log"
 	"net"
+	"time"
 
+	"github.com/barnabasSol/grpcsetup/client"
 	pb "github.com/barnabasSol/grpcsetup/coffeeshop_protos"
 	grpc "google.golang.org/grpc"
 )
@@ -14,24 +16,31 @@ type server struct {
 }
 
 func (s *server) GetMenu(mr *pb.MenuRequest, streamSrv grpc.ServerStreamingServer[pb.Menu]) error {
+	log.Print("GOT HIT")
 	items := []*pb.Item{
 		{Id: "1", Name: "Black Coffee"},
 		{Id: "2", Name: "Americano"},
 		{Id: "3", Name: "Iced Coffee"},
 	}
 	for i := range items {
-		streamSrv.Send(&pb.Menu{
-			Items: items[0 : i+1],
-		})
+		if streamSrv.Context().Err() != nil {
+			return streamSrv.Context().Err()
+		}
+		streamSrv.Send(&pb.Menu{Items: items[0 : i+1]})
+		time.Sleep(100 * time.Millisecond)
 	}
 	return nil
 }
+
 func (s *server) PlaceOrder(ctx context.Context, order *pb.Order) (*pb.Receipt, error) {
+	log.Println("place order hit")
 	return &pb.Receipt{
 		Id: "abc123",
 	}, nil
 }
+
 func (s *server) GetOrderStatus(ctx context.Context, receipt *pb.Receipt) (*pb.OrderStatus, error) {
+	log.Println("get order status hit")
 	return &pb.OrderStatus{
 		OrderId: receipt.Id,
 		Status:  "IN_PROGRESS",
@@ -39,7 +48,8 @@ func (s *server) GetOrderStatus(ctx context.Context, receipt *pb.Receipt) (*pb.O
 }
 
 func main() {
-	lis, err := net.Listen("tcp", "9001")
+	go client.RunClient()
+	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen %v", err)
 	}
