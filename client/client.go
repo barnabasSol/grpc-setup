7625133529db93aed27c94cd33d812048eeff207
barnabasSol <barnabassolomon336@gmail.com>
2025-06-13 22:59:17 +0300

    fixed client error, apparently connection is initiated asynchronously, a check is needed on its state to start communication

--- client/client.go
+++ client/client.go
@@ -9,26 +9,39 @@ import (
 
 	pb "github.com/barnabasSol/grpcsetup/coffeeshop_protos"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func RunClient() {
-	time.Sleep(3 * time.Second)
+	time.Sleep(2 * time.Second)
+
 	conn, err := grpc.NewClient(
-		"localhost:5001",
+		"localhost:50051",
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
 	if err != nil {
-		log.Fatalf("failed to connect to gRPC server %v", err)
+		log.Fatalf("failed to connect to gRPC server: %v", err)
 	}
 	defer conn.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	conn.Connect()
+	for conn.GetState() != connectivity.Ready {
+		if !conn.WaitForStateChange(ctx, conn.GetState()) {
+			log.Fatalf("connection timed out, last state: %v", conn.GetState())
+		}
+	}
+	log.Println("Connection state: Ready")
+
 	c := pb.NewCoffeeShopClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
 	menuStream, err := c.GetMenu(ctx, &pb.MenuRequest{})
 	if err != nil {
-		log.Fatalf("failed calling GetMenu %v", err)
+		log.Fatalf("failed calling GetMenu: %v", err)
 	}
 	done := make(chan bool)
 	var items []*pb.Item
@@ -40,16 +53,22 @@ func RunClient() {
 				return
 			}
 			if err != nil {
-				log.Fatalf("cannot receive from stream %v", err)
+				log.Fatalf("cannot receive from stream: %v", err)
 			}
 			items = resp.Items
+			fmt.Printf("items: %v\n", items)
 		}
 	}()
 	<-done
 	fmt.Printf("items: %v\n", items)
-	receipt, _ := c.PlaceOrder(ctx, &pb.Order{Items: items})
+	receipt, err := c.PlaceOrder(ctx, &pb.Order{Items: items})
+	if err != nil {
+		log.Fatalf("failed calling PlaceOrder: %v", err)
+	}
 	fmt.Printf("receipt: %v\n", receipt)
-	status, _ := c.GetOrderStatus(ctx, receipt)
+	status, err := c.GetOrderStatus(ctx, receipt)
+	if err != nil {
+		log.Fatalf("failed calling GetOrderStatus: %v", err)
+	}
 	fmt.Printf("status: %v\n", status)
-
 }
